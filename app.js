204b838e8a7d466cacdd8fb02285e17d741cfe0a
Gautham Ram <gthm.ram@gmail.com>
2023-08-07 11:24:34 +1000

    first lesson

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('here');
+const readLine = require('readline');
+
+let rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("what is your name", (name) => {
+    console.log("you entered: "+name);
+    rl.close();
+})
+
+rl.on("close", () => {
+   console.log("Interface is closed"); 
+});
